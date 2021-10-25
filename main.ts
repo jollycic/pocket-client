7f08cf0f8f377639fb2b0e26d94312e4b1cfbfe7
jollycic <jollycic@gmail.com>
2021-10-25 16:18:37 +0200

    Some code documentation

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+///<reference path="./types.d.ts" />
+
 import { IncomingMessage } from 'http'
 import { request } from 'https'
 
-export default class PocketClient implements PocketAPI {
+export default class PocketClient extends Pocket.API {
+    /**
+     * Creates a new Pocket (https://getpocket.com) API client
+     */
+    constructor ({ consumer_key, token, logger } : Pocket.APIConfig) {
+        super({ consumer_key, token, logger })
 
-    constructor ({ consumer_key, token, logger } : PocketAPIConfig) {
         this.consumer_key = consumer_key
         
         if (token) {
@@ -19,46 +25,17 @@ export default class PocketClient implements PocketAPI {
     consumer_key: string
     access_token: string
     username: string
-    requestToken: PocketRequestToken
+    requestToken: Pocket.RequestToken
     logger: Console
 
-    #buildRequest (path: string, data: any) {
-        if (typeof this.consumer_key === 'string') {
-            data.consumer_key = this.consumer_key
-        }
-
-        if (typeof this.access_token === 'string') {
-            data.access_token = this.access_token
-        }
-        
-        const options = {
-            host: this.host,
-            method: 'POST',
-            port: 443,
-            json: true,
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'X-Accept': 'application/json'
-            },
-            path,
-        }
-
-        return { options, payload: JSON.stringify(data) }
-    }
-
-    #logPocketError (response: IncomingMessage) : void {
-        this.logger.error(`${response.headers.status}
-        Pocket Error ${response.headers['x-error-code']}: ${response.headers['x-error']}`)
-    }
-
     requestAuthentication (redirect_uri: string) : Promise<URL> {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             const { options, payload } = this.#buildRequest('/v3/oauth/request', { redirect_uri })
 
             const req = request(options, (res) => {
                 res.on('data', (data) => {
                     if (res.statusCode === 200) {
-                        this.requestToken = JSON.parse(data) as PocketRequestToken
+                        this.requestToken = JSON.parse(data) as Pocket.RequestToken
                         const { code } = this.requestToken
                         resolve(new URL(`https://${this.host}/auth/authorize?request_token=${code}&redirect_uri=${redirect_uri}`))
                     } else {
@@ -77,18 +54,17 @@ export default class PocketClient implements PocketAPI {
 
             req.end()
         })
-
     }
 
-    authorize () : Promise<PocketAccessToken> {
+    authorize () : Promise<Pocket.AccessToken> {
         const { code } = this.requestToken
 
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             const { options, payload } = this.#buildRequest('/v3/oauth/authorize', { code })
             const req = request(options, (res) => {
                 res.on('data', (data) => {
                     if (res.statusCode === 200) {
-                        const accessToken = JSON.parse(data) as PocketAccessToken
+                        const accessToken = JSON.parse(data) as Pocket.AccessToken
                         this.access_token = accessToken.access_token
                         this.username = accessToken.username
                         resolve(accessToken)
@@ -100,7 +76,8 @@ export default class PocketClient implements PocketAPI {
             })
 
             req.on('error', (err) => {
-                reject(err)
+                this.logger.error(err)
+                resolve(null)
             })
 
             req.write(payload)
@@ -109,12 +86,12 @@ export default class PocketClient implements PocketAPI {
         })
     }
 
-    add (article: PocketAdd) : Promise<PocketListItem> {
+    add (article: Pocket.Addable) : Promise<Pocket.ListItem> {
         return new Promise((resolve, reject) => {
             const { options, payload } = this.#buildRequest('/v3/add', article)
             const req = request(options, (res) => {
                 res.on('data', ({ item }) => {
-                    resolve(JSON.parse(item) as PocketListItem)
+                    resolve(JSON.parse(item) as Pocket.ListItem)
                 })
             })
     
@@ -126,5 +103,51 @@ export default class PocketClient implements PocketAPI {
     
             req.end()
         })
+    }
+
+    // PRIVATE METHODS
+
+    /**
+     * @private Builds the options and the payload to perform a valid JSON request
+     * to the Pocket API
+     * @param path Pocket API endpoint
+     * @param data Payload to POST
+     * @returns options and payload for the API Request
+     * 
+     */
+     #buildRequest (path: string, data: any) : Pocket.RequestData {
+        if (typeof this.consumer_key === 'string') {
+            data.consumer_key = this.consumer_key
+        }
+
+        if (typeof this.access_token === 'string') {
+            data.access_token = this.access_token
+        }
+        
+        const options = {
+            host: this.host,
+            method: 'POST',
+            port: 443,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'X-Accept': 'application/json'
+            },
+            path: path.startsWith('/') ? path : '/' + path,
+        }
+
+        const payload = JSON.stringify(data)
+
+        return { options, payload }
+    }
+
+    /**
+     * @private Logs the relevant information about an error from the headers 
+     * in the response message from the Pocket API. See https://getpocket.com/developer/docs/errors
+     * @param response HTTPS response message from the Pocket API
+     */
+    #logPocketError (response: IncomingMessage) : void {
+        this.logger.error(`${response.headers.status}
+        Pocket Error ${response.headers['x-error-code']}: ${response.headers['x-error']}`)
     }
 }
--- types.d.ts
+++ types.d.ts
@@ -1,88 +1,106 @@
-/** Response to a successful request to the authentication endpoint */
-type PocketRequestToken = {
-    code: string
-}
+declare namespace Pocket {
+    /** Options and payload for a JSON request */
+    type RequestData = {
+        /** Request options @todo learn how to import the correct type from http */
+        options: any,
+        /** stringified JSON payload */
+        payload: string
+    }
 
-/** Response to a successful request to the authorization endpoint */
-type PocketAccessToken = {
-    /** Pocket Username for display purposes */
-    username: string
-    /** Pocket Application Access Token to be used in each authorized request */
-    access_token: string
-}
+    /** Response to a successful request to the authentication endpoint */
+    type RequestToken = {
+        code: string
+    }
 
-/** Data to save a single item */
-type PocketAdd = {
-    /** The URL of the item to save */
-    url: string
-    /** Ignored if Pocket detects a title from the content at the URL */
-    title?: string
-    /** Comma-separated list of tags to be applied to the item */
-    tags?: string
-}
+    /** Response to a successful request to the authorization endpoint */
+    type AccessToken = {
+        /** Pocket Username for display purposes */
+        username: string
+        /** Pocket Application Access Token to be used in each authorized request */
+        access_token: string
+    }
 
-/** Item in a Pocket List */
-type PocketListItem = {
-    item_id: string | number
-    resolved_id: string | number
-    normal_url: string
-    resolved_url: string
-    domain_id: string | number
-    origin_domain_id: string | number
-    response_code: string | number
-    mime_type: string
-    content_length: string | number
-    eocoding: string
-    date_resolved: string
-    date_published: string
-    title: string
-    excerpt: string
-    word_count: string | number
-    has_image: 0 | 1 | 2
-    has_video: 0 | 1 | 2
-    is_index: 0 | 1
-    is_article: 0 | 1
-    authors: string[]
-    images: string[]
-    videos: string[]
-}
+    /** Data to save a single item */
+    type Addable = {
+        /** The URL of the item to save */
+        url: string
+        /** Ignored if Pocket detects a title from the content at the URL */
+        title?: string
+        /** Comma-separated list of tags to be applied to the item */
+        tags?: string
+    }
 
-type PocketAPIConfig = {
-    consumer_key: string,
-    token?: PocketAccessToken,
-    logger?: Console
-}
+    /** Item in a Pocket List */
+    type ListItem = {
+        item_id: string | number
+        resolved_id: string | number
+        normal_url: string
+        resolved_url: string
+        domain_id: string | number
+        origin_domain_id: string | number
+        response_code: string | number
+        mime_type: string
+        content_length: string | number
+        eocoding: string
+        date_resolved: string
+        date_published: string
+        title: string
+        excerpt: string
+        word_count: string | number
+        has_image: 0 | 1 | 2
+        has_video: 0 | 1 | 2
+        is_index: 0 | 1
+        is_article: 0 | 1
+        authors: string[]
+        images: string[]
+        videos: string[]
+    }
 
-interface PocketAPI {
-    /** Pocket Application Consumer Key, used in each request */
-    consumer_key: string
-    /** Stored Pocket Authentication Request Token, used to authorize the User  */
-    requestToken: PocketRequestToken
-    /** Pocket Application Access Token to be used in each authorized request */
-    access_token: string
-    /** Pocket Username for display purposes */
-    username: string
+    /** Configuration object to initialize a new instance of the Pocket API client */
+    type APIConfig = {
+        /** Application Consumer Key */
+        consumer_key: string,
+        /** Previously stored Access Token and Username if the User has already authorized the application */
+        token?: AccessToken,
+        /** Logger object */
+        logger?: Console
+    }
 
-    /**
-     * Request authentication to the Pocket API: @see https://getpocket.com/developer/docs/authentication,
-     * step 2
-     * @param redirect_uri URI to redirect the User to once authenticate
-     * @returns Pocket URL to redirect the User to in order to authenticate
-     */
-    requestAuthentication(redirect_uri: string): Promise<URL>
+    class API {
+        constructor (config : APIConfig)
 
-    /**
-     * Once the User has authenticated to Pocket and granted permission to ypur App, 
-     * complete the authorization process: @see https://getpocket.com/developer/docs/authentication,
-     * step 5
-     * @returns Pocket Access Token and Pocket Username
-     */
-    authorize(): Promise<PocketAccessToken>
+        /** @readonly Pocket host for API requests */
+        get host() : string
+        /** Pocket Application Consumer Key, used in each request */
+        consumer_key: string
+        /** Stored Pocket Authentication Request Token, used to authorize the User  */
+        requestToken: RequestToken
+        /** Pocket Application Access Token to be used in each authorized request */
+        access_token: string
+        /** Pocket Username for display purposes */
+        username: string
 
-    /**
-     * Add an Article to the User’s Pocket List
-     * @param payload Article to add
-     * @returns Added article
-     */
-    add(article: PocketAdd): Promise<PocketListItem>
+        /**
+         * Requests authentication to the Pocket API as per https://getpocket.com/developer/docs/authentication
+         * at step 2
+         * @param redirect_uri URI to redirect the User to once authenticate
+         * @returns Pocket URL to redirect the User to in order to authenticate
+         */
+        requestAuthentication(redirect_uri: string) : Promise<URL>
+
+        /**
+         * Once the User has authenticated to Pocket and granted permission to your App, 
+         * completes the authorization process as per https://getpocket.com/developer/docs/authentication
+         * at step 5
+         * @returns Pocket Access Token and Pocket Username
+         */
+        authorize() : Promise<AccessToken>
+
+        /**
+         * Add an Article to the User’s Pocket List
+         * @param payload Article to add
+         * @returns Added article
+         */
+        add(article: Addable) : Promise<ListItem>
+    }
 }
